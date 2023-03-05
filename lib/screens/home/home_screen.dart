b174857452c32ec77ce46c5b61c008817d93a0d4
hungit <105206566+hungit@users.noreply.github.com>
2023-03-06 01:12:03 +0700

    flutter carousel_slider

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -1,5 +1,8 @@
+import 'package:carousel_slider/carousel_slider.dart';
+import 'package:e_commerce/models/models.dart';
 import 'package:e_commerce/widgets/widgets.dart';
 import 'package:flutter/material.dart';
+
 
 class HomeScreen extends StatelessWidget {
   static const String routeName = '/';
@@ -14,9 +17,22 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      appBar: CustomAppBar(title: "Zero to Hero",),
-      bottomNavigationBar: CustomNavBar(),
+    return Scaffold(
+      appBar: const CustomAppBar(title: "Zero to Hero",),
+      bottomNavigationBar: const CustomNavBar(),
+      body: CarouselSlider(
+        options: CarouselOptions(
+          aspectRatio: 1.5,
+          viewportFraction: 0.9,
+          enlargeCenterPage: true,
+          enlargeStrategy: CenterPageEnlargeStrategy.height,
+        ),
+        // items: imageSliders,
+        items: Category.categories.map(
+          (category) => HeroCarouselSlider(category: category,)
+        ).toList(),
+      ),
     );
   }
 }
+
