2d13416510fced1409c6ea932a5421740e61228a
hungnguyen <hungnv@hamogroups.com>
2023-03-19 11:50:57 +0700

    update bắt sự kiện text form

--- lib/screens/checkout/checkout_screen.dart
+++ lib/screens/checkout/checkout_screen.dart
@@ -1,69 +1,108 @@
-
+import 'package:e_commerce/blocs/blocs.dart';
 import 'package:e_commerce/widgets/widgets.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class CheckoutScreen extends StatelessWidget {
   static const String routeName = '/checkout';
 
   const CheckoutScreen({super.key});
 
-  static Route route(){
+  static Route route() {
     return MaterialPageRoute(
         settings: const RouteSettings(name: routeName),
-        builder: (_) => const CheckoutScreen()
-    );
+        builder: (_) => const CheckoutScreen());
   }
-
 
   @override
   Widget build(BuildContext context) {
-    final TextEditingController emailController = TextEditingController();
-    final TextEditingController nameController = TextEditingController();
-    final TextEditingController addressController = TextEditingController();
-    final TextEditingController cityController = TextEditingController();
-    final TextEditingController countryController = TextEditingController();
-    final TextEditingController zipCodeController = TextEditingController();
-
     return Scaffold(
-      appBar: const CustomAppBar(
-        title: "Check out",
-      ),
-      bottomNavigationBar: const CustomNavBar(screen: routeName,),
-      body: Padding(
-        padding: const EdgeInsets.all(20.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              'CUSTOMER INFORMATION',
-              style: Theme.of(context).textTheme.displaySmall,
-            ),
-            _buildTextFormField(emailController, context, 'Email'),
-            _buildTextFormField(nameController, context, 'Full Name'),
-            Text(
-              'DELIVERY INFORMATION',
-              style: Theme.of(context).textTheme.displaySmall,
-            ),
-            _buildTextFormField(addressController, context, 'Address'),
-            _buildTextFormField(cityController, context, 'City'),
-            _buildTextFormField(countryController, context, 'Country'),
-            _buildTextFormField(zipCodeController, context, 'Zip Code'),
-            Text(
-              'ORDER SUMMARY',
-              style: Theme.of(context).textTheme.displaySmall,
-            ),
-          ],
+        appBar: const CustomAppBar(
+          title: "Check out",
         ),
-      )
-    );
+        bottomNavigationBar: const CustomNavBar(
+          screen: routeName,
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: BlocBuilder<CheckoutBloc, CheckoutState>(
+            builder: (context, state) {
+              if(state is CheckoutLoading){
+                return const Center(
+                  child: CircularProgressIndicator(),
+                );
+              }
+              if(state is CheckoutLoaded) {
+                return Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'CUSTOMER INFORMATION',
+                      style: Theme
+                          .of(context)
+                          .textTheme
+                          .displaySmall,
+                    ),
+                    _buildTextFormField((value) {
+                      context.read<CheckoutBloc>().add(
+                        UpdateCheckout(email: value)
+                      );
+                    }, context, 'Email'),
+                    _buildTextFormField((value) {
+                      context.read<CheckoutBloc>().add(
+                          UpdateCheckout(fullName: value)
+                      );
+                    }, context, 'Full Name'),
+                    Text(
+                      'DELIVERY INFORMATION',
+                      style: Theme
+                          .of(context)
+                          .textTheme
+                          .displaySmall,
+                    ),
+                    _buildTextFormField((value) {
+                      context.read<CheckoutBloc>().add(
+                          UpdateCheckout(address: value)
+                      );
+                    }, context, 'Address'),
+                    _buildTextFormField((value) {
+                      context.read<CheckoutBloc>().add(
+                          UpdateCheckout(city: value)
+                      );
+                    }, context, 'City'),
+                    _buildTextFormField((value) {
+                      context.read<CheckoutBloc>().add(
+                          UpdateCheckout(country: value)
+                      );
+                    }, context, 'Country'),
+                    _buildTextFormField((value) {
+                      context.read<CheckoutBloc>().add(
+                          UpdateCheckout(zipCode: value)
+                      );
+                    }, context, 'Zip Code'),
+                    Text(
+                      'ORDER SUMMARY',
+                      style: Theme
+                          .of(context)
+                          .textTheme
+                          .displaySmall,
+                    ),
+                  ],
+                );
+              } else {
+                return const Text('Something were wrong');
+              }
+            },
+          ),
+        ));
   }
 
   Padding _buildTextFormField(
-    TextEditingController controller,
+    Function(String)? onChanged,
     BuildContext context,
     String labelText,
-  ){
+  ) {
     return Padding(
       padding: const EdgeInsets.all(8.0),
       child: Row(
@@ -76,19 +115,16 @@ class CheckoutScreen extends StatelessWidget {
             ),
           ),
           Expanded(
-            child: TextFormField(
-              controller: controller,
-              decoration: const InputDecoration(
+              child: TextFormField(
+            onChanged: onChanged,
+            decoration: const InputDecoration(
                 isDense: true,
                 contentPadding: EdgeInsets.only(left: 10),
                 focusedBorder: UnderlineInputBorder(
-                  borderSide: BorderSide(
-                    color: Colors.black,
-                  )
-                )
-              ),
-            )
-          ),
+                    borderSide: BorderSide(
+                  color: Colors.black,
+                ))),
+          )),
         ],
       ),
     );
