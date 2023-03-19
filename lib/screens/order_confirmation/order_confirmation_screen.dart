223bfbfa32ef92e53af44d9f829818541ed0e703
hungit <105206566+hungit@users.noreply.github.com>
2023-03-19 21:51:48 +0700

    màn hình order confirmation

--- lib/screens/order_confirmation/order_confirmation_screen.dart
+++ lib/screens/order_confirmation/order_confirmation_screen.dart
@@ -1,5 +1,6 @@
 import 'package:e_commerce/widgets/widgets.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
 class OrderConfirmation extends StatelessWidget {
   static const String routeName = '/order-confirmation';
@@ -25,10 +26,29 @@ class OrderConfirmation extends StatelessWidget {
         body: SingleChildScrollView(
           child: Column(
             children: [
-              Container(
-                color: Colors.black,
-                width: double.infinity,
-                height: 300,
+              Stack(
+                children: [
+                  Container(
+                    color: Colors.black,
+                    width: double.infinity,
+                    height: 300,
+                  ),
+                  Positioned(
+                    top: 125,
+                    left: (MediaQuery.of(context).size.width - 100) / 2,
+                    child: SvgPicture.asset('assets/svgs/FloralMandala.svg')
+                  ),
+                  Positioned(
+                    top: 250,
+                    height: 100,
+                    width: MediaQuery.of(context).size.width,
+                    child: Text(
+                      'Your order is completed!',
+                      textAlign: TextAlign.center,
+                      style: Theme.of(context).textTheme.displaySmall!.copyWith(color: Colors.white),
+                    ),
+                  ),
+                ],
               )
             ],
           ),
