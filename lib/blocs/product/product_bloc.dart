84f3bba62a6904ad24cba8bf523517782a845538
hungit <105206566+hungit@users.noreply.github.com>
2023-03-19 09:56:20 +0700

    product bloc

--- lib/blocs/product/product_bloc.dart
+++ lib/blocs/product/product_bloc.dart
@@ -1,3 +1,7 @@
+import 'dart:async';
+
+import 'package:e_commerce/models/models.dart';
+import 'package:e_commerce/repositories/repositories.dart';
 import 'package:equatable/equatable.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -5,8 +9,23 @@ part 'product_event.dart';
 part 'product_state.dart';
 
 class ProductBloc extends Bloc<ProductEvent, ProductState> {
-  ProductBloc() : super(ProductInitial()) {
-    on<ProductEvent>((event, emit) {
-    });
+  final ProductRepository _productRepository;
+  StreamSubscription? _productSubscription;
+
+  ProductBloc({required ProductRepository productRepository})
+      : _productRepository = productRepository, super(ProductLoading()) {
+    on<LoadProducts>(_loadCategoriesToState);
+    on<UpdateProducts>(_updateCategoriesToState);
+  }
+
+  void _loadCategoriesToState(LoadProducts event, Emitter<ProductState> emit){
+    _productSubscription?.cancel();
+    _productSubscription = _productRepository
+        .getAllProducts()
+        .listen((categories)=>add(UpdateProducts(categories)));
+  }
+
+  void _updateCategoriesToState(UpdateProducts event, Emitter<ProductState> emit){
+    ProductLoaded(categories: event.products);
   }
 }
--- lib/blocs/product/product_state.dart
+++ lib/blocs/product/product_state.dart
@@ -2,9 +2,18 @@ part of 'product_bloc.dart';
 
 abstract class ProductState extends Equatable {
   const ProductState();
-}
 
-class ProductInitial extends ProductState {
   @override
   List<Object> get props => [];
+}
+
+class ProductLoading extends ProductState {}
+
+class ProductLoaded extends ProductState {
+  final List<Product> categories;
+
+  const ProductLoaded({this.categories = const <Product>[]});
+
+  @override
+  List<Object> get props => [categories];
 }
