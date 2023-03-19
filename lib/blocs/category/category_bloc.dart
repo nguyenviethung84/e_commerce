94e8daf600b4b7d17ae3498de58751b4301c1d5f
hungnguyen <hungnv@hamogroups.com>
2023-03-19 15:02:35 +0700

    update category bloc

--- lib/blocs/category/category_bloc.dart
+++ lib/blocs/category/category_bloc.dart
@@ -6,27 +6,29 @@ import 'package:equatable/equatable.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 part 'category_event.dart';
-part 'category_state.dart';
 
+part 'category_state.dart';
 
 class CategoryBloc extends Bloc<CategoryEvent, CategoryState> {
   final CategoryRepository _categoryRepository;
   StreamSubscription? _categorySubscription;
 
   CategoryBloc({required CategoryRepository categoryRepository})
-      : _categoryRepository = categoryRepository, super(CategoryLoading()) {
-    on<LoadCategories>(_loadCategoriesToState);
-    on<UpdateCategories>(_updateCategoriesToState);
+      : _categoryRepository = categoryRepository,
+        super(CategoryLoading()) {
+    on<LoadCategories>(_onLoadCategories);
+    on<UpdateCategories>(_onUpdateCategories);
   }
 
-  void _loadCategoriesToState(LoadCategories event, Emitter<CategoryState> emit){
+  void _onLoadCategories(LoadCategories event, Emitter<CategoryState> emit) {
     _categorySubscription?.cancel();
     _categorySubscription = _categoryRepository
-    .getAllCategories()
-    .listen((categories)=>add(UpdateCategories(categories)));
+        .getAllCategories()
+        .listen((categories) => add(UpdateCategories(categories)));
   }
 
-  void _updateCategoriesToState(UpdateCategories event, Emitter<CategoryState> emit){
-    CategoryLoaded(categories: event.categories);
+  void _onUpdateCategories(
+      UpdateCategories event, Emitter<CategoryState> emit) {
+    emit(CategoryLoaded(categories: event.categories));
   }
 }
