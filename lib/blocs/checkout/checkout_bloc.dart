ad2f379ceee2820954bc0920927cde733afd37b0
hungnguyen <hungnv@hamogroups.com>
2023-03-19 16:12:49 +0700

    update checkout bloc

--- lib/blocs/checkout/checkout_bloc.dart
+++ lib/blocs/checkout/checkout_bloc.dart
@@ -7,6 +7,7 @@ import 'package:e_commerce/repositories/repositories.dart';
 import 'package:equatable/equatable.dart';
 
 part 'checkout_event.dart';
+
 part 'checkout_state.dart';
 
 class CheckoutBloc extends Bloc<CheckoutEvent, CheckoutState> {
@@ -18,51 +19,57 @@ class CheckoutBloc extends Bloc<CheckoutEvent, CheckoutState> {
   CheckoutBloc({
     required CartBloc cartBloc,
     required CheckoutRepository checkoutRepository,
-  }) : _cartBloc = cartBloc,
-  _checkoutRepository = checkoutRepository,
-    super(
-        cartBloc.state is CartLoaded
-          ? CheckoutLoaded(
+  })
+      : _cartBloc = cartBloc,
+        _checkoutRepository = checkoutRepository,
+        super(
+          cartBloc.state is CartLoaded
+              ? CheckoutLoaded(
             products: (cartBloc.state as CartLoaded).cart.products,
             subtotal: (cartBloc.state as CartLoaded).cart.subtotalString,
             deliveryFee: (cartBloc.state as CartLoaded).cart.freeDeliveryString,
             total: (cartBloc.state as CartLoaded).cart.totalString,
           )
-          : CheckoutLoading()
-    ) {
-      _cartSubscription = cartBloc.stream.listen((state) {
-        if(state is CartLoaded){
-          add(UpdateCheckout(cart: state.cart));
-        }
-      });
-      // on<UpdateCheckout>(_onUpdateCheckout as EventHandler<UpdateCheckout, CheckoutState>);
-      // on<ConfirmCheckout>(_onConfirmCheckout as EventHandler<ConfirmCheckout, CheckoutState>);
+              : CheckoutLoading()
+      ) {
+    on<UpdateCheckout>(_onUpdateCheckout);
+    on<ConfirmCheckout>(_onConfirmCheckout);
+    _cartSubscription = cartBloc.stream.listen((state) {
+      if (state is CartLoaded) {
+        add(UpdateCheckout(cart: state.cart));
+      }
+    });
   }
 
-  void _onUpdateCheckout(UpdateCheckout event, CheckoutState state){
-    if(state is CheckoutLoaded){
-      CheckoutLoaded(
-        fullName: event.fullName ?? state.fullName,
-        email: event.email ?? state.email,
-        address: event.address ?? state.address,
-        city: event.city ?? state.city,
-        country: event.country ?? state.country,
-        zipCode: event.zipCode ?? state.zipCode,
-        products: event.cart?.products ?? state.products,
-        subtotal: event.cart?.subtotalString ?? state.subtotal,
-        deliveryFee: event.cart?.deliveryFeeString ?? state.deliveryFee,
-        total: event.cart?.totalString ?? state.total,
+  void _onUpdateCheckout(UpdateCheckout event, Emitter<CheckoutState> emit) {
+    final state = this.state;
+    if (state is CheckoutLoaded) {
+      emit(
+          CheckoutLoaded(
+            fullName: event.fullName ?? state.fullName,
+            email: event.email ?? state.email,
+            address: event.address ?? state.address,
+            city: event.city ?? state.city,
+            country: event.country ?? state.country,
+            zipCode: event.zipCode ?? state.zipCode,
+            products: event.cart?.products ?? state.products,
+            subtotal: event.cart?.subtotalString ?? state.subtotal,
+            deliveryFee: event.cart?.deliveryFeeString ?? state.deliveryFee,
+            total: event.cart?.totalString ?? state.total,
+          )
       );
     }
   }
 
-  Future<void> _onConfirmCheckout(ConfirmCheckout event, CheckoutState state) async {
-    if(state is CheckoutLoaded){
-      try{
+  Future<void> _onConfirmCheckout(ConfirmCheckout event,
+      Emitter<CheckoutState> emit) async {
+    final state = this.state;
+    if (state is CheckoutLoaded) {
+      try {
         await _checkoutRepository.addCheckout(event.checkout);
         print('Done');
-        CheckoutLoading();
-      }catch (_){}
+        emit(CheckoutLoading());
+      } catch (_) {}
     }
   }
 }
